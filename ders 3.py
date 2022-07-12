bfd5ed0282dc81364a35ec9d342f89cae65869e1
berkaycvsgl <99661459+berkaycvsgl@users.noreply.github.com>
2022-07-12 16:25:12 +0300

    Create ders 3.py

---
+++ ders 3.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('../advanced lane line/lena.jpg')
+
+def click_event(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(x, ', ', y)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        strXY = str(x) + ', '+ str(y)
+        cv2.putText(img, strXY, (x,y), font, 0.5, (255, 0, 0), 2)
+        cv2.imshow('lena', img)
+    if event == cv2.EVENT_RBUTTONDOWN:
+        blue = img[y, x, 0]
+        green = img[y, x, 1]
+        red = img[y, x, 2]
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        strBGR = str(blue) + ', ' + str(green) + ', ' + str(red)
+        cv2.putText(img, strBGR, (x, y), font, .5, (0, 255, 255), 2)
+        cv2.imshow('lena', img)
+
+#img = np.zeros((512,512,3), np.uint8)
+#img = cv2.imread('lena.jpg')
+cv2.imshow('lena', img)
+
+cv2.setMouseCallback('lena', click_event)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
